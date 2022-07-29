d014a7282e7f0f3e1ab8c1ca91ee7e577eb35cc6
Antonio Sarosi <sarosiantonio@gmail.com>
2022-07-29 19:03:09 +0200

    Refactor Todo component

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -13,14 +13,12 @@
     </section>
 
     <section>
-      <div v-for="todo in todos" class="todo" :key="todo.id">
-        <p>{{ todo.title }}</p>
-        <div>
-          <button @click="removeTodo(todo)" class="remove-todo-btn">
-            &times;
-          </button>
-        </div>
-      </div>
+      <Todo
+        v-for="todo in todos"
+        :key="todo.id"
+        :title="todo.title"
+        @remove="removeTodo(todo.id)"
+      />
     </section>
   </main>
 </template>
@@ -29,13 +27,15 @@
 import Alert from "./components/Alert.vue";
 import Navbar from "./components/Navbar.vue";
 import AddTodoForm from "./components/AddTodoForm.vue";
+import Todo from "./components/Todo.vue";
 
 export default {
   components: {
     Alert,
     Navbar,
-    AddTodoForm
-},
+    AddTodoForm,
+    Todo,
+  },
 
   data() {
     return {
@@ -56,32 +56,9 @@ export default {
         id: Math.floor(Math.random() * 1000),
       });
     },
-    removeTodo(todoTitle) {
-      this.todos = this.todos.filter((todo) => todo !== todoTitle);
+    removeTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
     },
   },
 };
 </script>
-
-<style scoped>
-.todo {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background: var(--accent-color);
-  margin-top: 30px;
-  padding: 0 20px 0 20px;
-  border-radius: 10px;
-}
-
-.remove-todo-btn {
-  border-radius: 50%;
-  border: none;
-  height: 40px;
-  width: 40px;
-  font-size: 30px;
-  color: var(--text-color);
-  background: var(--danger-color);
-  cursor: pointer;
-}
-</style>
---
+++ todo-list/src/components/Todo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="todo">
+    <p>{{ title }}</p>
+    <div>
+      <button @click="$emit('remove')" class="remove-todo-btn">&times;</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: true,
+      type: String,
+    },
+  },
+
+  emits: ['remove']
+};
+</script>
+
+<style scoped>
+.todo {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--accent-color);
+  margin-top: 30px;
+  padding: 0 20px 0 20px;
+  border-radius: 10px;
+}
+
+.remove-todo-btn {
+  border-radius: 50%;
+  border: none;
+  height: 40px;
+  width: 40px;
+  font-size: 30px;
+  color: var(--text-color);
+  background: var(--danger-color);
+  cursor: pointer;
+}
+</style>
